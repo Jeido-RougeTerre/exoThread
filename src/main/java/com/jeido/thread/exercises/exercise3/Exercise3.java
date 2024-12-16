95f39b4134bfac5e841be602361facfe1ce91c5c
TeoGon <wampawither@gmail.com>
2024-12-16 14:10:47 +0100

    ex3 - correction

--- src/main/java/com/jeido/thread/exercises/exercise3/Exercise3.java
+++ src/main/java/com/jeido/thread/exercises/exercise3/Exercise3.java
@@ -5,11 +5,16 @@ import java.util.concurrent.*;
 public class Exercise3 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        for (int i = 1; i < 6; i++) {
-            CalculatorCallable calculatorCallable = new CalculatorCallable(i);
-
-            Future<Integer> result = executor.submit(calculatorCallable);
-            System.out.println("Le cube de " + i + " est " + result.get());
+        Future<Integer>[] futures = new Future[5];
+        for (int i = 1; i <= 5; i++) {
+            futures[i - 1] = executor.submit(new CalculatorCallable(i));
+        }
+        for (int i = 0; i < futures.length; i++) {
+            try {
+                System.out.println("Le cube de " + (i + 1) + " est " + futures[i].get());
+            } catch (InterruptedException | ExecutionException e) {
+                System.out.println("Erreur de execution pour " + i);
+            }
         }
         executor.shutdown();
 
