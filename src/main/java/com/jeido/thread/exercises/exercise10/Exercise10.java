ced5d0e3fc1543f8349b117499d7af7569b39b18
TeoGon <wampawither@gmail.com>
2024-12-17 14:28:29 +0100

    ex10 - correction

--- src/main/java/com/jeido/thread/exercises/exercise10/Exercise10.java
+++ src/main/java/com/jeido/thread/exercises/exercise10/Exercise10.java
@@ -8,12 +8,10 @@ public class Exercise10 {
         Thread[] threads = new Thread[5];
         for (int i = 1; i <= threads.length; i++) {
             int mult = i + 1;
-            long res = mult * atoVal.get();
-            atoVal.set(res);
+            long res = atoVal.updateAndGet(v -> mult * v);
             threads[i - 1] = new Thread(() -> System.out.println(Thread.currentThread().getName() + " multiplicateur : " + mult + " = " + res));
+            threads[i].start();
         }
-
-        for (Thread thread : threads) thread.start();
         for (Thread thread : threads) thread.join();
 
         System.out.println("Valeur finale : " + atoVal.get());
