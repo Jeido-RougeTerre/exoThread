1796b3a61c861a99af85b788bfa671ff0861a060
TeoGon <wampawither@gmail.com>
2024-12-18 10:06:10 +0100

    ex11 - with synchronized method

--- src/main/java/com/jeido/thread/exercises/exercise11/BankAccount.java
+++ src/main/java/com/jeido/thread/exercises/exercise11/BankAccount.java
@@ -17,7 +17,7 @@ public class BankAccount {
         return balance;
     }
 
-    public void deposit(double amount) {
+    public synchronized void deposit(double amount) {
         this.balance += amount;
         System.out.printf("%s deposited %s%.2f$%s, current balance : %.2f$%n",
                 Thread.currentThread().getName(),
@@ -28,27 +28,28 @@ public class BankAccount {
         );
     }
 
-    public void withdraw(double amount) {
-        if (this.balance < amount)
+    public synchronized void withdraw(double amount) {
+        if (this.balance < amount) {
             System.out.printf(
-                "%s%s couldn't withdraw %s%.2f$%s (Insufficient funds). Current balance : %.2f$%s%n",
-                Ansi.RED.getCode(),
-                Thread.currentThread().getName(),
-                Ansi.RED_BOLD_BRIGHT.getCode(),
-                amount,
-                Ansi.RESET.getCode() + Ansi.RED.getCode(),
-                this.balance,
-                Ansi.RESET.getCode()
+                    "%s%s couldn't withdraw %s%.2f$%s (Insufficient funds). Current balance : %.2f$%s%n",
+                    Ansi.RED.getCode(),
+                    Thread.currentThread().getName(),
+                    Ansi.RED_BOLD_BRIGHT.getCode(),
+                    amount,
+                    Ansi.RESET.getCode() + Ansi.RED.getCode(),
+                    this.balance,
+                    Ansi.RESET.getCode()
             );
-
-        this.balance -= amount;
-        System.out.printf("%s withdraw %s%.2f$%s, current balance : %.2f$%n",
-                Thread.currentThread().getName(),
-                Ansi.RED.getCode(),
-                amount,
-                Ansi.RESET.getCode(),
-                this.balance
-        );
+        } else {
+            this.balance -= amount;
+            System.out.printf("%s withdraw %s%.2f$%s, current balance : %.2f$%n",
+                    Thread.currentThread().getName(),
+                    Ansi.RED.getCode(),
+                    amount,
+                    Ansi.RESET.getCode(),
+                    this.balance
+            );
+        }
     }
 
 }
--- src/main/java/com/jeido/thread/exercises/exercise11/Exercise11.java
+++ src/main/java/com/jeido/thread/exercises/exercise11/Exercise11.java
@@ -2,36 +2,25 @@ package com.jeido.thread.exercises.exercise11;
 
 import com.jeido.thread.ui.Ansi;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Exercise11 {
-    private static final Object lock = new Object();
     public static void main(String[] args) throws InterruptedException {
         BankAccount ba = new BankAccount();
 
-        Thread thread1 = new Thread(() -> {
-            synchronized (lock) {
-                for (int i = 0; i < 5; i++) {
-                    ba.deposit(10);
-                }
+        Runnable depositTask = () -> {
+            for (int i = 0; i < 5; i++) {
+                ba.deposit(10);
             }
-        });
+        };
 
-        Thread thread2 = new Thread(() -> {
-            synchronized (lock) {
-                for (int i = 0; i < 5; i++) {
-                    ba.withdraw(10);
-                }
+        Runnable withdrawTask = () -> {
+            for (int i = 0; i < 5; i++) {
+                ba.withdraw(10);
             }
-        });
+        };
 
-        Thread thread3 = new Thread(() -> {
-            synchronized (lock) {
-                for (int i = 0; i < 5; i++) {
-                    ba.deposit(10);
-                }
-            }
-        });
+        Thread thread1 = new Thread(depositTask);
+        Thread thread2 = new Thread(withdrawTask);
+        Thread thread3 = new Thread(depositTask);
 
         thread1.start();
         thread2.start();
