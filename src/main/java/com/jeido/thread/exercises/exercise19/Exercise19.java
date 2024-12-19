c451c72cb2ee9d76b9837561aca9889521878ea0
Jeido <teo.gontard@gmail.com>
2024-12-19 13:47:11 +0100

    ex 19

---
+++ src/main/java/com/jeido/thread/exercises/exercise19/Exercise19.java
@@ -0,0 +1,41 @@
+package com.jeido.thread.exercises.exercise19;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Exercise19 {
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
+        Runnable addTask = () -> {
+            for (int i = 0; i < 10; i++) {
+                String item = "Producer-Element-" + i;
+                queue.add(item);
+                System.out.println("Producer added: " + item);
+            }
+        };
+
+        Runnable removeTask = () -> {
+            for (int i = 0; i < 10; i++) {
+                String item = "Producer-Element-" + i;
+                if (queue.contains(item)) {
+                    queue.remove(item);
+                    System.out.println("Consumer removed: " + item);
+                } else {
+                    System.out.println("Consumer could not remove: " + item);
+                }
+            }
+        };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(addTask);
+        executorService.submit(removeTask);
+        executorService.shutdown();
+
+        while (!executorService.isTerminated()) {
+            Thread.sleep(100);
+        }
+
+        System.out.println("Queue : " + queue);
+    }
+}
